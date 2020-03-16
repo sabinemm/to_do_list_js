97dc6132eb79fa67462fd3be7649ed7e944604ca
Sabine <sabine@21.lv>
2020-03-16 21:07:23 +0100

    missing elements, code not working

--- scripts.js
+++ scripts.js
@@ -1,60 +1,116 @@
-const clear = document.querySelector(".clear") //selecting clear button 
+// selecting elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
 
-const dateElement = document.getElementById("date")
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 
-const list = document.getElementById("list")
+let LIST, id;
 
-const CHECK = "fa-check-circle"
+let data = localStorage.getItem("TODO"); // get items from local storage
 
-function addToDo(toDo) {
-    const text = <li class="item">
-        <i class="far fa-circle" job="complete"></i>
-        <p class="text "> ${toDo} </p>
-        <i class="far fa-trash-alt" job="delete"></i>
-    </li>
-
-    const position = "beforeend"
-
-    list.insertAdjacentHTML(position, text);
+// is it empty?
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+} else {
+    // if data isn't empty
+    LIST = [];
+    id = 0;
 }
 
-document.addEventListener("keyup", function (event)) {
-    if (event.keycode == 13) {
+// add items
+function loadList(array) {
+    array.forEach(function (item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+// clear
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// date
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// add to do function
+
+function addToDo(toDo, id, done, trash) {
+
+    if (trash) { return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+// add an item to the list
+document.addEventListener("keyup", function (even) {
+    if (event.keyCode == 13) {
         const toDo = input.value;
+
         if (toDo) {
             addToDo(toDo, id, false, false);
-            LIST.push(
-                {
-                    name: toDo,
-                    id: id,
-                    done: false,
-                    trash: false;
-                }
-            );
-            input.value = "";
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
             id++;
         }
+        input.value = "";
     }
 });
 
-let LIST = [{}, {}, ...];
 
-let id = 0;
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
 
-LIST[0] ->
-
-{
-    name: "Drink Coffee",
-    id: 0,
-    done: false,
-    trash: false
+    LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
-LIST[0] ->
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
 
-{
-    name: "Workout",
-    id: 1,
-    done: true,
-    trash: false
+    LIST[element.id].trash = true;
 }
+
+
+list.addEventListener("click", function (event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob == "complete") {
+        completeToDo(element);
+    } else if (elementJob == "delete") {
+        removeToDo(element);
+    }
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
